853d377d99a4628759a3a8bc90d29a64a6e01d10
Rancor38 <zak.rancor@hotmail.com>
2022-08-29 18:43:59 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Bio from './components/Bio';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+      <Routes>
+        <Route path='/' exact element={<Bio />} />
+      </Routes>
+    </>
   );
 }
 
