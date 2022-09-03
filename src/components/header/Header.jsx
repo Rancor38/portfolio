68e574bee7bb8b305934fea7cc55414c037bd944
Rancor38 <zak.rancor@hotmail.com>
2022-09-02 18:32:22 -0700

    playing with ico

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,20 +6,23 @@ const Header = () => {
         <>
             {/* <h1 id='title'>Zakariah O'Mara-Mezzano</h1> */}
             <header>
-            <img src=''
-            <a className='menuButton' href='/'> <motion.h1
+                <a className='menuButton' href='/'> <motion.img className='logo'
+                        whileHover={{ scale: 1.1 }}
+                        whileTap={{ scale: 0.9 }}
+                        src='https://github.com/Rancor38/portfolio/blob/main/public/logos/Z-Logo.png?raw=true' alt='Z-logo'/> </a>
+                <a className='menuButton' href='/'> <motion.h1
                 whileHover={{ scale: 1.1 }}
                 whileTap={{ scale: 0.9 }}
-            >Bio</motion.h1> </a>
-            <a className='menuButton' href='/projects'> <motion.h1
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-            >Projects</motion.h1> </a>
-            <a className='menuButton' href='/resume'> <motion.h1
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-            >Resume</motion.h1> </a>
-            </header>
+                >Bio</motion.h1> </a>
+                <a className='menuButton' href='/projects'> <motion.h1
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                >Projects</motion.h1> </a>
+                <a className='menuButton' href='/resume'> <motion.h1
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                >Resume</motion.h1> </a>
+                </header>
         </>
     );
 };
