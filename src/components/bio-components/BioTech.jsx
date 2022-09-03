dc90b3e1df836062f3a8462f4b7f2fa78114b9ea
Rancor38 <zak.rancor@hotmail.com>
2022-09-02 17:07:11 -0700

    trying to extract images

--- src/components/bio-components/BioTech.jsx
+++ src/components/bio-components/BioTech.jsx
@@ -1,24 +1,44 @@
 import React from 'react';
-import html from './logos/html-5.svg'
-import css from './logos/css-3.svg'
-import js from './logos/javascript.svg'
+// import aws from '../../../public/logos/aws-elastic-beakstalk.svg'
+// import css from './logos/css-3.svg'
+// import django from './logos/django-icon.svg'
+// import express from './logos/express.svg'
+// import html from './logos/html-5.svg'
+// import javascript from './logos/javascript.svg'
+// import jira from './logos/jira.svg'
+// import mysql from './logos/mysql-icon.svg'
+// import mongodb from './logos/mongodb-icon.svg'
+// import node from './logos/nodejs-icon.svg'
+// import postgresql from './logos/postgresql.svg'
+// import postman from './logos/postman-icon.svg'
+// import python from './logos/python.svg'
+// import react from './logos/react.svg'
+
+import techObject from './logoObjects/logos.js'
+
 import { motion } from 'framer-motion'
 
 const BioTech = () => {
+
+    const index = []
+    const findMyIndex = (techObject) => {
+        for (let i = 0;
+            i < techObject.length;
+            i += 1) {
+            index.push(techObject[i])
+            }
+    }
+
     return (
         <ul className='bioTech'>
-            <motion.li whileHover={{ scale: 1.01 }} >
+            {findMyIndex(techObject)}
+            {console.log(index)}
+            {index.map((logo) => (
+                <motion.li whileHover={{ scale: 1.25 }} >
                 <img
-                    className='logo' src={html} alt='html-5' />
-            </motion.li>
-            <motion.li whileHover={{ scale: 1.01 }} >
-                <img
-                    className='logo' src={css} alt='css-3' />
-            </motion.li>
-            <motion.li whileHover={{ scale: 1.01 }} >
-                <img
-                    className='logo' src={js} alt='JavaScript' />
-            </motion.li>
+                    className='logo' src={logo.logo} alt={logo.name} />
+                 </motion.li>
+            ))}
         </ul>
     );
 };
---
+++ src/components/bio-components/logoObjects/logos.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const logos = [
+    {
+        "name": "aws",
+        "logo": "../../../public/logos/aws-elastic-beakstalk.svg"
+    },
+    {
+        "name": "css",
+        "logo": "src/components/bio-components/logos/css-3.svg"
+    },
+    {
+        "name": "django",
+        "logo": "src/components/bio-components/logos/express.svg"
+    },
+    {
+        "name": "html",
+        "logo": "src/components/bio-components/logos/html-5.svg"
+    },
+    {
+        "name": "savascript",
+        "logo": "src/components/bio-components/logos/javascript.svg"
+    },
+    {
+        "name": "jira",
+        "logo": "src/components/bio-components/logos/jira.svg"
+    },
+    {
+        "name": "microsoft",
+        "logo": "src/components/bio-components/logos/microsoft-icon.svg"
+    },
+    {
+        "name": "mongodb",
+        "logo": "src/components/bio-components/logos/mongodb-icon.svg"
+    },
+    {
+        "name": "mysql",
+        "logo": "src/components/bio-components/logos/mysql-icon.svg"
+    },
+    {
+        "name": "node",
+        "logo": "src/components/bio-components/logos/nodejs-icon.svg"
+    },
+    {
+        "name": "postgresql",
+        "logo": "src/components/bio-components/logos/postgresql.svg"
+    },
+    {
+        "name": "postman",
+        "logo": "src/components/bio-components/logos/postman-icon.svg"
+    },
+    {
+        "name": "python",
+        "logo": "src/components/bio-components/logos/python.svg"
+    },
+    {
+        "name": "react",
+        "logo": "src/components/bio-components/logos/react.svg"
+    }
+]
+
+export default logos;
