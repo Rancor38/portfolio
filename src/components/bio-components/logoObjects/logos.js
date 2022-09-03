317fae68bdd0be69516141ac7ff8ff2deefd177f
Rancor38 <zak.rancor@hotmail.com>
2022-09-02 17:54:32 -0700

    Updating tech logo

--- src/components/bio-components/logoObjects/logos.js
+++ src/components/bio-components/logoObjects/logos.js
@@ -1,8 +1,8 @@
 const logoJsonArr = [
     {
-        "name": "aws",
-        "logo": "https://raw.githubusercontent.com/Rancor38/portfolio/dc90b3e1df836062f3a8462f4b7f2fa78114b9ea/public/logos/aws-elastic-beanstalk.svg",
-        "learnMore": "https://aws.amazon.com/elasticbeanstalk/"
+        "name": "html",
+        "logo": "https://raw.githubusercontent.com/Rancor38/portfolio/dc90b3e1df836062f3a8462f4b7f2fa78114b9ea/public/logos//html-5.svg",
+        "learnMore": "https://html.spec.whatwg.org/multipage/"
     },
     {
         "name": "css",
@@ -10,19 +10,29 @@ const logoJsonArr = [
         "learnMore": "https://www.w3.org/TR/CSS/"
     },
     {
-        "name": "django",
-        "logo": "https://raw.githubusercontent.com/Rancor38/portfolio/dc90b3e1df836062f3a8462f4b7f2fa78114b9ea/public/logos//express.svg",
-        "learnMore": "https://www.djangoproject.com/"
-    },
-    {
-        "name": "html",
-        "logo": "https://raw.githubusercontent.com/Rancor38/portfolio/dc90b3e1df836062f3a8462f4b7f2fa78114b9ea/public/logos//html-5.svg",
-        "learnMore": "https://html.spec.whatwg.org/multipage/"
-    },
-    {
         "name": "javascript",
         "logo": "https://raw.githubusercontent.com/Rancor38/portfolio/dc90b3e1df836062f3a8462f4b7f2fa78114b9ea/public/logos//javascript.svg",
         "learnMore": "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+    },
+    {
+        "name": "python",
+        "logo": "https://raw.githubusercontent.com/Rancor38/portfolio/dc90b3e1df836062f3a8462f4b7f2fa78114b9ea/public/logos/python.svg",
+        "learnMore": "https://www.python.org/"
+    },
+    {
+        "name": "react",
+         "logo": "https://raw.githubusercontent.com/Rancor38/portfolio/dc90b3e1df836062f3a8462f4b7f2fa78114b9ea/public/logos/react.svg",
+         "learnMore": "https://reactjs.org/"
+    },
+    {
+        "name": "django",
+        "logo": "https://raw.githubusercontent.com/Rancor38/portfolio/dc90b3e1df836062f3a8462f4b7f2fa78114b9ea/public/logos/django-icon.svg",
+        "learnMore": "https://www.djangoproject.com/"
+    },
+    {
+        "name": "node",
+        "logo": "https://raw.githubusercontent.com/Rancor38/portfolio/dc90b3e1df836062f3a8462f4b7f2fa78114b9ea/public/logos/nodejs-icon.svg",
+        "learnMore": "https://nodejs.org/en/"
     },
     {
         "name": "jira",
@@ -30,9 +40,9 @@ const logoJsonArr = [
         "learnMore": "https://www.atlassian.com/software/jira"
     },
     {
-        "name": "microsoft",
-        "logo": "https://raw.githubusercontent.com/Rancor38/portfolio/dc90b3e1df836062f3a8462f4b7f2fa78114b9ea/public/logos/microsoft-icon.svg",
-        "learnMore": "https://www.techwalla.com/articles/what-is-microsoft-office-suite"
+        "name": "postgresql",
+        "logo": "https://raw.githubusercontent.com/Rancor38/portfolio/dc90b3e1df836062f3a8462f4b7f2fa78114b9ea/public/logos/postgresql.svg",
+        "learnMore": "https://www.postgresql.org/"
     },
     {
         "name": "mongodb",
@@ -45,29 +55,24 @@ const logoJsonArr = [
         "learnMore": "https://dev.mysql.com/"
     },
     {
-        "name": "node",
-        "logo": "https://raw.githubusercontent.com/Rancor38/portfolio/dc90b3e1df836062f3a8462f4b7f2fa78114b9ea/public/logos/nodejs-icon.svg",
-        "learnMore": "https://nodejs.org/en/"
-    },
-    {
-        "name": "postgresql",
-        "logo": "https://raw.githubusercontent.com/Rancor38/portfolio/dc90b3e1df836062f3a8462f4b7f2fa78114b9ea/public/logos/postgresql.svg",
-        "learnMore": "https://www.postgresql.org/"
-    },
-    {
         "name": "postman",
         "logo": "https://raw.githubusercontent.com/Rancor38/portfolio/dc90b3e1df836062f3a8462f4b7f2fa78114b9ea/public/logos/postman-icon.svg",
         "learnMore": "https://www.postman.com/"
     },
     {
-        "name": "python",
-        "logo": "https://raw.githubusercontent.com/Rancor38/portfolio/dc90b3e1df836062f3a8462f4b7f2fa78114b9ea/public/logos/python.svg",
-        "learnMore": "https://www.python.org/"
+        "name": "aws",
+        "logo": "https://raw.githubusercontent.com/Rancor38/portfolio/dc90b3e1df836062f3a8462f4b7f2fa78114b9ea/public/logos/aws-elastic-beanstalk.svg",
+        "learnMore": "https://aws.amazon.com/elasticbeanstalk/"
     },
     {
-        "name": "react",
-        "logo": "https://raw.githubusercontent.com/Rancor38/portfolio/dc90b3e1df836062f3a8462f4b7f2fa78114b9ea/public/logos/react.svg",
-        "learnMore": "https://reactjs.org/"
+        "name": "microsoft",
+        "logo": "https://raw.githubusercontent.com/Rancor38/portfolio/dc90b3e1df836062f3a8462f4b7f2fa78114b9ea/public/logos/microsoft-icon.svg",
+        "learnMore": "https://www.techwalla.com/articles/what-is-microsoft-office-suite"
+    },
+    {
+        "name": "express",
+        "logo": "https://raw.githubusercontent.com/Rancor38/portfolio/dc90b3e1df836062f3a8462f4b7f2fa78114b9ea/public/logos/express-icon.svg",
+        "learnMore": "https://expressjs.com/"
     }
 ]
 
