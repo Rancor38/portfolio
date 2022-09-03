77cbc13ec67a7c30561a80d352be4a2e9332df39
Rancor38 <zak.rancor@hotmail.com>
2022-09-03 16:36:38 -0700

    Added stylings to resume

--- src/components/projects-components/Projects.jsx
+++ src/components/projects-components/Projects.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Slideshow from './Slideshow';
-import './styles.css'
 
 const Projects = () => {
 
--- src/components/projects-components/Slideshow.jsx
+++ src/components/projects-components/Slideshow.jsx
@@ -1,29 +1,30 @@
 import React, { useState } from 'react';
 import { images } from './imageObjects/image-data';
 import { AnimatePresence, motion } from 'framer-motion'
+import './styles.css'
 
 const variants = {
   intial: direction => {
     return {
-      x: direction > 0 ? 200 : -200,
-      opacity: 0
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0,
     }
   },
   animate: {
     x: 0,
     opacity: 1,
     trasition: {
-      x: { type: 'spring', stiffness: 300, damping: 20},
+      x: { type: "spring", stiffness: 300, damping: 30},
       opacity: { duration: 0.2 },
     }
   },
   exit: direction => {
     return {
-      x: direction >0 ? -200 : 200,
+      x: direction > 0 ? -1000 : 1000,
       opacity: 0,
       trasition: {
-        x: { type: 'spring', stiffness: 300, damping: 20 },
-        opacity: { duration: 0.2 }
+        x: { type: "spring", stiffness: 100, damping: 20 },
+        opacity: { duration: 0.2 },
       }
     }
   },
@@ -56,24 +57,30 @@ const Slideshow = () => {
     <div className="container">
       <div className="slideshow">
         <AnimatePresence initial={false} custom={direction}>
-          <motion.div variants={variants}
+            
+          <motion.a
+            variants={variants}
             animate='animate'
             initial='initial'
             exit='exit'
             key={images[index]}
-            custom={direction}>
-              <a href={images[index].url} target='_blank' rel='noreferrer'>
-                <img
+            custom={direction}
+            href={images[index].url}
+            target='_blank'
+            rel='noreferrer'>
+            <motion.img
+              whileHover={{ scale: 1.01 }}
                   src={images[index].image}
                   alt="slides"
                   className="slides"
               />
-              </a>
-          </motion.div>
+              </motion.a>
           
           </AnimatePresence>
-        <button className='prevButton slideShowButton' onClick={prevStep}>◀️</button>
-        <button className='nextButton slideShowButton' onClick={nextStep}>▶️</button>
+        <motion.button whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }} className='prevButton slideShowButton' onClick={prevStep}>◀️</motion.button>
+        <motion.button whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }} className='nextButton slideShowButton' onClick={nextStep}>▶️</motion.button>
     </div>
 </div>
   );
