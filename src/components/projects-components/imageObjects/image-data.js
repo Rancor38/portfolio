cc95e4e712f42c32977c6909ef1f26f2347fbe94
Rancor38 <zak.rancor@hotmail.com>
2022-09-04 01:50:43 -0700

    Changed project image data to route to correct urls

--- src/components/projects-components/imageObjects/image-data.js
+++ src/components/projects-components/imageObjects/image-data.js
@@ -1,14 +1,14 @@
 export const images = [
   {
     "image": "https://github.com/Rancor38/portfolio/blob/main/public/project-images/EZ-Notes.png?raw=true",
-    "url": "https://github.com/ez-notes/frontend"
+    "url" : "https://github.com/ez-notes/frontend"
   },  
   {
     "image": "https://github.com/Rancor38/portfolio/blob/main/public/project-images/Monster-Finder.png?raw=true",
-    "url": "https://github.com/ez-notes/frontend"
+    "url" : "https://github.com/Rancor38/monster"
   }, 
   {
     "image": "https://github.com/Rancor38/portfolio/blob/main/public/project-images/Tic-tac-toe.png?raw=true",
-    "url": "https://github.com/Rancor38/monster"
+    "url" : "https://github.com/Rancor38/tic-tac-toe"
   }, 
 ];
