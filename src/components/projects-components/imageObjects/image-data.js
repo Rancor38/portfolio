d19d2d71465b5664a66f0011bb94b8413bca49c9
Rancor38 <zak.rancor@hotmail.com>
2022-09-20 18:04:17 -0700

    Reordered list

--- src/components/projects-components/imageObjects/image-data.js
+++ src/components/projects-components/imageObjects/image-data.js
@@ -4,13 +4,13 @@ export const images = [
     "url" : "https://github.com/ez-notes/frontend"
   },  
   {
-    "image": "https://github.com/Rancor38/portfolio/blob/main/public/project-images/Monster-Finder.png?raw=true",
-    "url" : "https://github.com/Rancor38/monster"
-  }, 
-  {
     "image": "https://user-images.githubusercontent.com/105610124/191386507-17a0f41f-ee3d-49e4-ad27-3098e03b3e3c.png",
     "url" : "https://github.com/Rancor38/rpg-dice-roller"
-  }, 
+  },
+  {
+    "image": "https://github.com/Rancor38/portfolio/blob/main/public/project-images/Monster-Finder.png?raw=true",
+    "url" : "https://github.com/Rancor38/monster"
+  },  
   {
     "image": "https://github.com/Rancor38/portfolio/blob/main/public/project-images/Tic-tac-toe.png?raw=true",
     "url" : "https://github.com/Rancor38/tic-tac-toe"
