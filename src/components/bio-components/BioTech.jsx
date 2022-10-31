a581a341dbbb1a1390bff467133284ede87e64ea
Rancor38 <zak.rancor@hotmail.com>
2022-10-31 14:11:28 -0700

    removed commented test code

--- src/components/bio-components/BioTech.jsx
+++ src/components/bio-components/BioTech.jsx
@@ -1,19 +1,4 @@
 import React from 'react';
-// import aws from '../../../public/logos/aws-elastic-beakstalk.svg'
-// import css from './logos/css-3.svg'
-// import django from './logos/django-icon.svg'
-// import express from './logos/express.svg'
-// import html from './logos/html-5.svg'
-// import javascript from './logos/javascript.svg'
-// import jira from './logos/jira.svg'
-// import mysql from './logos/mysql-icon.svg'
-// import mongodb from './logos/mongodb-icon.svg'
-// import node from './logos/nodejs-icon.svg'
-// import postgresql from './logos/postgresql.svg'
-// import postman from './logos/postman-icon.svg'
-// import python from './logos/python.svg'
-// import react from './logos/react.svg'
-
 import techObject from './logoObjects/logos.js'
 
 import { motion } from 'framer-motion'
