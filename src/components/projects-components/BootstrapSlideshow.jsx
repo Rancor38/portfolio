8c89325817a3e51a3eaffd51cfccc27ceb39fb9f
Rancor38 <zak.rancor@hotmail.com>
2022-12-02 21:06:47 -0800

    Added Captions as link to repos. Refactor

--- src/components/projects-components/BootstrapSlideshow.jsx
+++ src/components/projects-components/BootstrapSlideshow.jsx
@@ -35,17 +35,23 @@ const BootstrapSlideshow = () => {
     } else {
         const eachItem = images.list.map((element, imageIndex) => 
             <Carousel.Item> 
-                <img
-                    className='dblock w-100'
-                    src={images.list[imageIndex].image}
-                    alt={imageIndex}
-                    />
-                {/* <Carousel.Caption>
+                    <img
+                        className='dblock w-100'
+                        src={images.list[imageIndex].image}
+                        alt={imageIndex}
+                        />
+                <Carousel.Caption>
+                    
+                    <a href={images.list[imageIndex].url}
+                        target='_blank'
+                        rel='noreferrer'>
                         <h1>
                             {images.list[imageIndex].name}
                         </h1>
-                </Carousel.Caption> */}
-            </Carousel.Item>
+                    </a>
+
+                </Carousel.Caption>
+            </Carousel.Item>    
             // <img
             //     className='dblock w-100'
             //     src={images.list[image].image}
