ad7b63f3c61663698c607e32653f6de0dde95f03
Rancor38 <zak.rancor@hotmail.com>
2022-12-16 12:31:45 -0800

    removed 'recently'

--- src/components/bio-components/BioTagline.jsx
+++ src/components/bio-components/BioTagline.jsx
@@ -10,7 +10,7 @@ const BioTagline = () => {
     return (
         <span className='bioTagline'>
             <h5>
-                Hello! I'm Zakariah! I'm a creative soul that loves building things, and I recently learned software engineering at <motion.button className='GA' whileHover={{ scale: 1.05 }}
+                Hello! I'm Zakariah! I'm a creative soul that loves building things, and I learned software engineering at <motion.button className='GA' whileHover={{ scale: 1.05 }}
                         whileTap={{ scale: 0.9 }} onClick={routMe}>General Assembly!</motion.button>
             </h5>
             <img className='avatar' src='https://avatars.githubusercontent.com/u/105610124?v=4' alt='zakariah' />
