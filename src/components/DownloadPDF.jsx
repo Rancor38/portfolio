87681813c8f233363720baaf4502e8d5299bc85f
Rancor38 <zak.rancor@hotmail.com>
2024-04-16 18:24:53 -0700

    Add Resume Confirm and changes to skills

--- src/components/DownloadPDF.jsx
+++ src/components/DownloadPDF.jsx
@@ -1,23 +1,35 @@
 function DownloadPDF() {
   function handleDownload() {
-    // Path to the file to be downloaded
-    const fileURL = process.env.PUBLIC_URL + "/resumePDF/Zakariah_Software_Developer_Resume.pdf";
+    // Display a confirmation dialog
+    const isConfirmed = window.confirm("Download latest resume?");
+    
+    // If user confirms, proceed with download
+    if (isConfirmed) {
+      // Path to the file to be downloaded
+      const fileURL = process.env.PUBLIC_URL + "/resumePDF/resume.pdf";
+      
       // Create a temporary anchor element
-    const downloadLink = document.createElement("a");
+      const downloadLink = document.createElement("a");
+      
       // Set the href attribute to the file's URL
-    downloadLink.href = fileURL;
+      downloadLink.href = fileURL;
+      
       // Set the download attribute to the file's name
-    downloadLink.download = "Zakariah_Software_Developer_Resume.pdf";
+      downloadLink.download = "Zakariah's_Resume.pdf";
+      
       // Append the anchor element to the document body
-    document.body.appendChild(downloadLink);
+      document.body.appendChild(downloadLink);
+      
       // Click the download link to start the download
-    downloadLink.click();
+      downloadLink.click();
+      
       // Remove the temporary anchor element
-    document.body.removeChild(downloadLink);
-    // Open the downloaded file in a new window
-    window.open(fileURL, "_blank");
+      document.body.removeChild(downloadLink);
+      
+      // Open the downloaded file in a new window
+      window.open(fileURL, "_blank");
+    }
   }
-  
 
   return (
     <>
